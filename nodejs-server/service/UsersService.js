ea4cf4d46b737060106c7e813b88bb4c23d6407f
SwaggerHub <SwaggerHub@smartbear.com>
2020-07-12 17:54:23 +0000

    Initial commit from SwaggerHub

---
+++ nodejs-server/service/UsersService.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+/**
+ * Get all currently created users.
+ *
+ * no response value expected for this operation
+ **/
+exports.getUsers = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
